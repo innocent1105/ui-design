6dd0895f6c43f80d39f1b5a246bd4c72665162dd
asmita.shenvi <asmita.shenvi@mobisoftinfotech.com>
2025-04-01 10:05:31 +0530

    Updated the typography changes

--- src/pages/Typography/Typography.tsx
+++ src/pages/Typography/Typography.tsx
@@ -11,21 +11,19 @@ export default function Typography() {
 				/>
 				<div className="grid gap-4 ">
 					<MainCard title="Body Copy">
-						<p className="mb-4 text-card-foreground">
+						<p className="mb-4 text-lg">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor.
 							Aenean massa.
 						</p>
-						<p className="text-sm text-card-foreground">
+						<p className="text-sm">
 							Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus
 							ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
 							tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
 							Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante,
 							dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
 							laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur
-							ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.
-						</p>
-						<p className="mt-4 text-sm text-card-foreground">
-							Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante
+							ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget.Nam
+							quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante
 							tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam
 							sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.
 							Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus
