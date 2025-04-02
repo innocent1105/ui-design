3afaaac7cb9cd61567d5c893989b1df532bab93f
asmita.shenvi <asmita.shenvi@mobisoftinfotech.com>
2025-04-02 11:03:23 +0530

    Updated the scroll to table

--- src/pages/Dashborad/Dashboard.tsx
+++ src/pages/Dashborad/Dashboard.tsx
@@ -5,12 +5,12 @@ const Dashboard = () => {
 	return (
 		<div className="flex min-h-screen">
 			<main className="flex-1 space-y-4 p-1">
-				<PageHeader items={[ { label: 'Home', href: '/' }, { label: 'Dashboard', href: '/' } ]} />
-				<h1 className="heading text-4xl leading-14 !font-bold  tracking-tight">Hello Everyone!</h1>
-				{/* Dashboard Grid */}
-				<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-					{/* Grid Row 1 */}
-					<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 col-span-full min-h-[300px]">
+				<PageHeader
+					items={[ { label: 'Home', href: '/' }, { label: 'Dashboard', href: '/' } ]}
+					heading="Hello Everyone!"
+				/>
+				<div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+					<div className="col-span-full grid min-h-[300px] grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
 						<Card className="col-span-1 h-full">
 							<div className="p-4" />
 						</Card>
@@ -30,8 +30,7 @@ const Dashboard = () => {
 						</Card>
 					</div>
 
-					{/* Grid Row 2 */}
-					<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 col-span-full min-h-[150px]">
+					<div className="col-span-full grid min-h-[150px] grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
 						<Card className="col-span-1 h-full">
 							<div className="p-4" />
 						</Card>
--- src/pages/Tables/Tables.tsx
+++ src/pages/Tables/Tables.tsx
@@ -1,12 +1,12 @@
 import { DataTable } from '@/components/data-table';
 import { useState } from 'react';
 import { employees } from './data';
-import { PageBreadcrumb } from '@/components/page-breadcrumb';
+import { PageHeader } from '@/components/page-header';
 
 const ITEMS_PER_PAGE = 5;
 
 const Tables = () => {
-	const [ currentPage, setCurrentPage ] = useState(1);
+	const [currentPage, setCurrentPage] = useState(1);
 	const totalPages = Math.ceil(employees.length / ITEMS_PER_PAGE);
 	const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
 	const paginatedData = employees.slice(startIndex, startIndex + ITEMS_PER_PAGE);
@@ -14,8 +14,13 @@ const Tables = () => {
 	return (
 		<div className="flex">
 			<main className="flex-1 space-y-4 p-1">
-				<PageBreadcrumb items={[ { label: 'Home', href: '/' }, { label: 'Tables', href: '/tables' } ]} />
-				<h1 className="heading text-4xl leading-14 !font-bold  tracking-tight">Tables</h1>
+				<PageHeader
+					items={[
+						{ label: 'Home', href: '/' },
+						{ label: 'Tables', href: '/tables' }
+					]}
+					heading="Tables"
+				/>
 
 				<div>
 					<DataTable
