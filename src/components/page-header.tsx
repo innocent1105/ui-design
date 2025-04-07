65ae91a0a2059fa082abd6cd3b35bd002857c22a
asmita.shenvi <asmita.shenvi@mobisoftinfotech.com>
2025-04-07 18:30:58 +0530

    Updated the page header

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -17,9 +17,9 @@ export function PageHeader({ items, heading, children }: PageHeaderProps) {
 			<PageBreadcrumb items={items} />
 			<div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
 				<div className="flex items-center gap-2">
-					<h2 className="heading text-2xl leading-tight !font-bold tracking-tight sm:text-3xl sm:leading-14">
+					<h1 className="heading text-4xl leading-tight !font-bold tracking-tight sm:text-3xl sm:leading-14">
 						{heading}
-					</h2>
+					</h1>
 				</div>
 				<div className="flex w-full flex-col items-stretch gap-2 sm:w-auto sm:flex-row sm:items-center">
 					{children}
