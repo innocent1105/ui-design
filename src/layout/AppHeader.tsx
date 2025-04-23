017d8727eaeb4f0af50d64aab071092b92ca0909
asmita.shenvi <asmita.shenvi@mobisoftinfotech.com>
2025-04-23 16:24:51 +0530

    Updated the padding around theme button

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -20,9 +20,8 @@ const AppHeader: React.FC = () => {
 					</div>
 				</div>
 			</div>
-			<div className="flex grow flex-col items-center justify-end lg:flex-row lg:px-6">
+			<div className="flex grow flex-col items-center justify-end pr-2 lg:flex-row lg:px-6">
 				<Button
-					variant="ghost"
 					variantClassName="light"
 					size="icon"
 					onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
