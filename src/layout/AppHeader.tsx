6b6ae7ce779bd8c768d793089df4ad65b9baa00f
asmita.shenvi <asmita.shenvi@mobisoftinfotech.com>
2025-05-02 10:22:20 +0530

    Updated the header

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -3,25 +3,25 @@ import { Separator } from '@/components/ui/separator';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { useTheme } from '@/context/ThemeContext';
 import { MoonIcon, SunIcon } from 'lucide-react';
-import LogoLight from '@/assets/logo-light-theme.svg';
-import LogoDark from '@/assets/logo-dark-theme.svg';
+import CustomSearch from '@/components/custom-controls/custom-search';
+import { useState } from 'react';
 
 const AppHeader: React.FC = () => {
 	const { theme, setTheme } = useTheme();
+	const [ searchQuery, setSearchQuery ] = useState('');
+
+	const handleSearch = (value: string) => {
+		setSearchQuery(value);
+	};
+
 	return (
 		<header className="bg-background sticky top-0 z-10 flex w-full border-b-0 py-3">
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
 				<SidebarTrigger className="-ml-1 cursor-pointer" />
 				<Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
-
-				<div className="flex w-full items-center justify-center gap-2 sm:w-auto">
-					<img
-						src={theme !== 'dark' ? LogoDark : LogoLight}
-						alt="Logo Light"
-						className="block h-6 w-[150px]"
-					/>
-				</div>
+				<CustomSearch value={searchQuery} onChange={handleSearch} placeholder="Type keywords..." />
 			</div>
+
 			<div className="flex grow flex-col items-center justify-end pr-2 lg:flex-row lg:px-6">
 				<Button
 					variant="ghost"
